f2cddb7da86f546d6a0a889c8799b339045c9bc2
Christoffer Riis <Christoffer@riis.cc>
2023-03-23 09:31:23 +0100

    Added class for prepereShapes

---
+++ src/main/java/com/example/_3dengine/Shape3DClass.java
@@ -0,0 +1,51 @@
+package com.example._3dengine;
+
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Material;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Box;
+
+import java.util.Objects;
+
+public class Shape3DClass extends PhongMaterial {
+
+    PhongMaterial material;
+    Box box;
+
+    public Shape3DClass() {
+        box = prepereBox();
+
+    }
+
+    public Box getBox() {
+        return box;
+    }
+
+    private Box prepereBox() {
+        material = new PhongMaterial();
+        try {
+            material.setDiffuseMap(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/img/diffuseMap.jpg"))));
+            material.setBumpMap(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/img/normalMap.jpg"))));
+            material.setSpecularMap(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/img/specularMap.jpg"))));
+            material.setSelfIlluminationMap(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/img/illumination.png"))));
+        } catch (Exception e) {
+            System.out.println("Error while loading textures: " + e.toString());
+        } finally {
+            if (material.getDiffuseMap() == null) {
+                material.setDiffuseColor(Color.RED);
+            }
+            if (material.getBumpMap() == null) {
+                material.setSpecularColor(Color.BLUE);
+            }
+            if (material.getSpecularMap() == null) {
+                material.setSpecularColor(Color.GREEN);
+            }
+        }
+
+        Box box = new Box(200, 200, 200);
+        box.setMaterial(material);
+        return box;
+    }
+
+}
